7aa4a5d10c329a3525d731c8b872728a899efd2b
chellej80 <embarrequestrian@gmail.com>
2022-07-05 20:11:39 +0000

    created view for List of posts and added pagination

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import generic
+from .models import Post
+
+
+#Pagination limit amount of posts that appear on the page
+#Create view of listed posts 
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 6
