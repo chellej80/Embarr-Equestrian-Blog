c9347228e5e7f9922763bd1d22b801e4f1f0ad06
chellej80 <embarrequestrian@gmail.com>
2022-07-06 19:49:24 +0000

    updated settings py with allauth app ref

--- Embarr_Blog/settings.py
+++ Embarr_Blog/settings.py
@@ -42,9 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django_summernote',
     'blog',
 ]
