77abee669caa874f6a12cdd047f4815ac6718f84
chellej80 <embarrequestrian@gmail.com>
2022-07-06 19:52:11 +0000

    updated settings with site id and login redirects

--- Embarr_Blog/settings.py
+++ Embarr_Blog/settings.py
@@ -53,6 +53,13 @@ INSTALLED_APPS = [
     'blog',
 ]
 
+#added site id so django can handle multiple sites from one db
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
