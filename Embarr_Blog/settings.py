411a9f4291a675c206aa7aa0b5feb290ad318fb4
chellej80 <embarrequestrian@gmail.com>
2022-07-06 21:18:11 +0000

    updated settings apps with crispy

--- Embarr_Blog/settings.py
+++ Embarr_Blog/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -58,6 +59,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 #To prevent 500 errors during login and registration
 ACCOUNT_EMAIL_VERIFICATION = 'none'
