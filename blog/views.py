b0b826849d732e1f49d100dab87562f8aa266ba1
chellej80 <embarrequestrian@gmail.com>
2022-07-06 19:43:14 +0000

    installed allauth for authenication functions

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,6 @@ from django.views import generic, View
 from .models import Post
 
 
-#Pagination limit amount of posts that appear on the page
-#Create view of listed posts 
-
-
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
